95a665cdae2778c36847e245672d6863d6ba9d6e
Kim Nilsson <kim.nilsson@effnet.com>
2019-02-12 23:57:35 +0100

    Added dynamically rendered final line + right click cancellation

--- include/displaywidget.h
+++ include/displaywidget.h
@@ -10,6 +10,7 @@
 
 #include <glm/glm.hpp>
 
+#include <memory>
 #include <vector>
 
 class DisplayWidget : public QOpenGLWidget, protected QOpenGLFunctions
@@ -25,20 +26,47 @@ protected:
 	void paintGL();
 
 	void mousePressEvent(QMouseEvent* event);
+	void mouseMoveEvent(QMouseEvent* event);
 
 	QOpenGLContext* _context;
 
 private:
+	typedef glm::vec3 v3;
+	typedef std::shared_ptr<v3> v3_ptr;
+	typedef std::pair<v3_ptr, v3_ptr> line3;
+	typedef std::shared_ptr<line3> line3_ptr;
+
+	void addPoint(const v3& point);
+	void addLine(v3_ptr& a, v3_ptr& b);
+	void addPolygon(v3_ptr& p);
+
+	void updatePoints();
+
+	QOpenGLShaderProgram _prog;
+	QOpenGLBuffer _point_vbo;
+	QOpenGLBuffer _line_vbo;
+	QOpenGLBuffer _guide_vbo;
+	QOpenGLVertexArrayObject _point_vao;
+	QOpenGLVertexArrayObject _line_vao;
+	QOpenGLVertexArrayObject _guide_vao;
+
+	std::vector<v3_ptr> _points;
+	std::vector<line3_ptr> _lines;
+	std::unordered_map<v3_ptr, line3_ptr> _p2l;
+	std::unordered_map<line3_ptr, v3_ptr> _l2p;
+
 	glm::mat4 _proj;
 	glm::mat4 _modelview;
 	glm::vec4 _viewport;
+	glm::vec3 _mousePos;
+
+	v3_ptr _prev_point;
+
+	float _threshold;
+	float _mouse_threshold;
 
 	GLint _mvp_location;
 
-	QOpenGLBuffer _vbo;
-	QOpenGLVertexArrayObject _vao;
-	QOpenGLShaderProgram _prog;
-	std::vector<glm::vec2> _points;
 	int _width;
 	int _height;
 };
--- src/displaywidget.cpp
+++ src/displaywidget.cpp
@@ -4,13 +4,18 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include <iostream>
+#include <memory>
+#include <queue>
 
 DisplayWidget::DisplayWidget(QWidget* parent) :
 	QOpenGLWidget(parent),
-	_vbo(QOpenGLBuffer::VertexBuffer),
-	_modelview(1.0f)
+	_point_vbo(QOpenGLBuffer::VertexBuffer),
+	_modelview(1.0f),
+	_threshold(20),
+	_mouse_threshold(2)
 {
 	_proj = glm::ortho(-1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f);
+	setMouseTracking(true);
 }
 
 void DisplayWidget::initializeGL()
@@ -31,29 +36,45 @@ void DisplayWidget::initializeGL()
 	_prog.link();
 	_mvp_location = _prog.uniformLocation("mvp");
 
-	// Set up VAO
-	_vao.create();
+	// Set up VAOs
+	_point_vao.create();
+	_line_vao.create();
+	_guide_vao.create();
 
-	// Bind VAO
-	/*-------------------------*/
 	_prog.bind();
-	_vao.bind();
-	/*-------------------------*/
 
-	// Set up VBO
-	_vbo.create();
-	_vbo.setUsagePattern(QOpenGLBuffer::StreamDraw);
-	_vbo.bind();
-	_vbo.allocate(256*sizeof(glm::vec2));
-	_prog.setAttributeBuffer("in_position", GL_FLOAT, 0, 2);
-	_prog.enableAttributeArray("in_position");
-	_vbo.release();
+	// Set up VBOs
+	_point_vao.bind();
+		_point_vbo.create();
+		_point_vbo.setUsagePattern(QOpenGLBuffer::DynamicDraw);
+		_point_vbo.bind();
+			_point_vbo.allocate(256*sizeof(v3));
+			_prog.setAttributeBuffer("in_position", GL_FLOAT, 0, 3);
+			_prog.enableAttributeArray("in_position");
+		_point_vbo.release();
+	_point_vao.release();
 
-	// Unbind VAO
-	/*-------------------------*/
-	_vao.release();
+	_line_vao.bind();
+		_line_vbo.create();
+		_line_vbo.setUsagePattern(QOpenGLBuffer::DynamicDraw);
+		_line_vbo.bind();
+			_line_vbo.allocate(512*sizeof(v3));
+			_prog.setAttributeBuffer("in_position", GL_FLOAT, 0, 3);
+			_prog.enableAttributeArray("in_position");
+		_line_vbo.release();
+	_line_vao.release();
+
+	_guide_vao.bind();
+		_guide_vbo.create();
+		_guide_vbo.setUsagePattern(QOpenGLBuffer::DynamicDraw);
+		_guide_vbo.bind();
+			_guide_vbo.allocate(2*sizeof(v3));
+			_prog.setAttributeBuffer("in_position", GL_FLOAT, 0, 3);
+			_prog.enableAttributeArray("in_position");
+		_guide_vbo.release();
+	_guide_vao.release();
+
 	_prog.release();
-	/*-------------------------*/
 }
 
 void DisplayWidget::resizeGL(int w, int h)
@@ -66,58 +87,57 @@ void DisplayWidget::resizeGL(int w, int h)
 
 void DisplayWidget::paintGL()
 {
-	std::cout << "DisplayWidget::paintGL()" << std::endl;
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	_prog.bind();
 	auto mvp = _proj * _modelview;
 	glUniformMatrix4fv(_mvp_location, 1, GL_FALSE, glm::value_ptr(mvp));
 
-	// DRAW!
-	_vao.bind();
-	glDrawArrays(GL_POINTS, 0, _points.size());
-	glDrawArrays(GL_LINE_STRIP, 0, _points.size());
-	_vao.release();
+	_point_vao.bind();
+		glDrawArrays(GL_POINTS, 0, _points.size());
+	_point_vao.release();
+
+	_line_vao.bind();
+		glDrawArrays(GL_LINES, 0, 2 * _lines.size());
+	_line_vao.release();
+
+	if (_prev_point)
+	{
+		auto res = glm::unProject(_mousePos, _modelview, _proj, _viewport);
+		_guide_vbo.bind();
+			_guide_vbo.write(0, _prev_point.get(), sizeof(v3));
+			_guide_vbo.write(sizeof(v3), &res, sizeof(v3));
+		_guide_vbo.release();
+
+		_guide_vao.bind();
+			glDrawArrays(GL_LINES, 0, 2);
+		_guide_vao.release();
+	}
 
 	_prog.release();
 }
 
 void DisplayWidget::mousePressEvent(QMouseEvent* event)
 {
+	_mousePos = {event->x(), _height - event->y(), 0.0f};
+
 	switch(event->button())
 	{
 		case Qt::LeftButton:
 		{
-
-			// TODO: Lines from pairs of points and drawing with GL_LINE instead
+			// @TODO: Lines from pairs of points and drawing with GL_LINE instead
 			// Looping for closed polygon detection (relational point<->line?)
 			// Line intersection checking
-			// Automatic continuation with dynamically rendered final line
-			// + right click cancellation
 			// CGAL for tesselation?
-			glm::vec3 v(event->x(), _height - event->y(), 0.0f);
-			glm::vec3 res = glm::unProject(v, _modelview, _proj, _viewport);
+			// Repeat timer for continuous drawing?
 
-			auto size = _points.size();
-			_points.emplace_back(res.x, res.y);
-
-			_vbo.bind();
-			_vbo.write(size * sizeof(glm::vec2), &_points.back(), sizeof(glm::vec2));
-			_vbo.release();
-
-			std::cout << "------------" << std::endl;
-			for (auto& x : _points)
-			{
-				std::cout << x.x << " " << x.y << std::endl;
-
-			}
-			std::cout << "----- " << _points.size() << " -----" << std::endl;
+			addPoint(_mousePos);
 		}
 		break;
 
 		case Qt::RightButton:
 		{
-
+			_prev_point.reset();
 		}
 		break;
 
@@ -128,4 +148,99 @@ void DisplayWidget::mousePressEvent(QMouseEvent* event)
 	update();
 }
 
+void DisplayWidget::mouseMoveEvent(QMouseEvent* event)
+{
+	if (_prev_point)
+	{
+		glm::vec3 res(event->x(), _height - event->y(), 0.0f);
 
+		if (glm::length(_mousePos - res) > _mouse_threshold)
+		{
+			_mousePos = res;
+			update();
+		}
+	}
+}
+
+void DisplayWidget::addPoint(const glm::vec3& point)
+{
+	// @TODO: Quadtree
+
+	auto end = _points.end();
+	auto res = glm::unProject(point, _modelview, _proj, _viewport);
+
+	auto compare = [&](const v3_ptr& a, const v3_ptr& b)
+	{
+		return glm::length(res - *a) > glm::length(res - *b);
+	};
+
+	std::priority_queue<v3_ptr, std::vector<v3_ptr>, decltype(compare)> hits(_points.begin(), end, compare);
+	if (hits.empty())
+	{
+		_prev_point = std::make_shared<v3>(res);
+		_points.push_back(_prev_point);
+		updatePoints();
+
+		return;
+	}
+
+	auto max = hits.top();
+	res = glm::project(*max, _modelview, _proj, _viewport);
+	if (glm::length(point - res) < _threshold)
+	{
+		if (_prev_point)
+		{
+			if (max != _prev_point)
+			{
+				addLine(_prev_point, max);
+				_prev_point.reset();
+
+				addPolygon(max);
+			}
+		}
+		else
+		{
+			_prev_point = max;
+		}
+	}
+	else
+	{
+		auto res = glm::unProject(point, _modelview, _proj, _viewport);
+		auto new_point = std::make_shared<v3>(res);
+
+		if (_prev_point)
+		{
+			addLine(_prev_point, new_point);
+		}
+
+		_points.push_back(new_point);
+		_prev_point = new_point;
+		updatePoints();
+	}
+}
+
+void DisplayWidget::addLine(v3_ptr& a, v3_ptr& b)
+{
+	// @TODO: Add to unordered maps for indexing
+
+	auto size = _lines.size();
+	auto line = std::make_shared<line3>(a, b);
+	_lines.push_back(line);
+
+	_line_vbo.bind();
+		_line_vbo.write(2*size * sizeof(v3), a.get(), sizeof(v3));
+		_line_vbo.write((2*size + 1) * sizeof(v3), b.get(), sizeof(v3));
+	_line_vbo.release();
+}
+
+void DisplayWidget::addPolygon(v3_ptr &p)
+{
+	// @TODO: Walk polygon to find closed loop
+}
+
+void DisplayWidget::updatePoints()
+{
+	_point_vbo.bind();
+		_point_vbo.write((_points.size() - 1) * sizeof(v3), _points.back().get(), sizeof(v3));
+	_point_vbo.release();
+}
